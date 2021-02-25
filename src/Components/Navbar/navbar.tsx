d6b79415d17e3f5d3e68337696198b9d9038f46f
lukas <lukas.minnebeck@arcor.de>
2021-02-25 21:39:06 +0100

    1

---
+++ src/Components/Navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className={"navbar-container"}>
+            <nav className={"navbar-menu"}>
+                <li><a href="#"></a></li>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
