c9e278ad0fac4b2e693561f6d90a9a583e1fee7b
lukas <lukas.minnebeck@arcor.de>
2021-02-27 21:57:50 +0100

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -12,31 +12,16 @@ interface AppState {
 }
 
 interface AppProps {
-    general?: IGeneral,
 }
 
-@inject('general')
-@observer
+
 class App extends Component<AppProps, AppState> {
-    componentDidMount(){
-        this.updateHeight();
-        this.updateWidth();
-        window.addEventListener("resize", ()=>{this.props.general?.setWidth(window.innerWidth)});
-    }
 
-    updateHeight = () => {
-        this.props.general?.setHeight(window.innerHeight);
-    }
-
-    updateWidth = () => {
-        this.props.general?.setWidth(window.innerWidth);
-    }
 
     render() {
         return(
 
             <div>
-                <Header/>
                 <HeroSec/>
             </div>
         )
