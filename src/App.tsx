80c267cb3521b14139400c65dea1fd61089dae44
lukas <lukas.minnebeck@arcor.de>
2021-03-01 16:12:29 +0100

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,6 @@ class App extends Component<AppProps, AppState> {
         return (
 
             <div id={"app"}>
-                <Header/>
                 <HeroSec/>
                 <TestimonialSection/>
                 <GetStarted/>
