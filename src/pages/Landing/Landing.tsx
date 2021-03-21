965a7f8ce1f0841c972465d34fdf083b953169ea
JonaBecher <33008684+JonaBecher@users.noreply.github.com>
2021-03-21 22:07:13 +0100

    Update Landing.tsx

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -59,7 +59,7 @@ class Landing extends Component<AppProps, AppState> {
 
                             </div>
 
-                            <Features fheading_1={"Portfolio Performance"} fheading_2={"Diversification"}
+                            <Features fheading_1={"Portfolio Performance Test"} fheading_2={"Diversification"}
                                       fheading_3={"Portfolio Measures"}
                                       fsubheading_1={"Track the performance of all your holdings in one place"}
                                       fsubheading_2={"How well is your portfolio diversified among different dimensions"}
